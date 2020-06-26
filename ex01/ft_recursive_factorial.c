8569cae62294f110eb8825ee51a3d9ccdf2588e5
Rorisang Monatisa <rmonatis@c4r2s5.wethinkcode.co.za>
2020-06-26 09:05:08 +0200

    exercise01

--- ex01/ft_recursive_factorial.c
+++ ex01/ft_recursive_factorial.c
@@ -1,32 +1,16 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_iterative_factorial.c                           :+:      :+:    :+:   */
+/*   ft_recursive_factorial.c                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: rmonatis <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2020/06/25 10:09:45 by rmonatis          #+#    #+#             */
-/*   Updated: 2020/06/25 14:28:26 by rmonatis         ###   ########.fr       */
+/*   Created: 2020/06/25 15:58:19 by rmonatis          #+#    #+#             */
+/*   Updated: 2020/06/25 16:12:36 by rmonatis         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-int ft_iterative_factorial(int nb)
+int ft_recursive_factorial(int nd)
 {
-	int i = 1;
-	int tempi = 1;
-
-	if(nb < 0)
-	{
-		return(0);
-	}
-	if(nb <= 1)
-	{
-		return(1);
-	}
-	while(1 <= nb)
-	{
-		tempi = tempi * i;
-		i++;
-	}
-	return (tempi);
+	
 }
